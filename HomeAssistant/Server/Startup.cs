33415d4d3b983e4c39844dcffc27f4dcfef653cd
Arun <arun.prakash.456789@gmail.com>
2019-06-17 17:59:07 +0530

    Update Startup.cs

--- HomeAssistant/Server/Startup.cs
+++ HomeAssistant/Server/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using HomeAssistant.Core;
+using Swashbuckle.AspNetCore.Swagger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using Microsoft.OpenApi.Models;
 
 namespace HomeAssistant.Server {
 	public class Startup {
@@ -26,12 +28,34 @@ namespace HomeAssistant.Server {
 					options.SerializerSettings.Formatting = Formatting.Indented;
 				}
 			);
-            
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo {
+					Version = "v1",
+					Title = "Home assistant API",
+					Description = "Documentation for tess api endpoints.",
+					Contact = new OpenApiContact() {
+						Name = "Arun Prakash",
+						Email = "arun.prakash.456789@gmail.com",
+						Url = new Uri("https://github.com/SynergYFTW/HomeAssistant")
+					},
+					License = new OpenApiLicense() {
+						Name = "MIT License",
+						Url = new Uri("https://github.com/SynergYFTW/HomeAssistant/blob/master/LICENSE")
+					}
+				});
+			});
 		}
 
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
+			app.UseSwagger();
 			app.UseResponseCompression();
 			app.UseWebSockets();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "TESS api documentation");
+				c.RoutePrefix = string.Empty;
+			});
 			app.UseMvc();
 		}
 	}
