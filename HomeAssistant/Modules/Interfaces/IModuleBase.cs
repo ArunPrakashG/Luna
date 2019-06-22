f2f64456465708b263500e6e3556c60f61461c25
Arun <arun.prakash.456789@gmail.com>
2019-06-22 19:45:00 +0530

    Update IModuleBase.cs

--- HomeAssistant/Modules/Interfaces/IModuleBase.cs
+++ HomeAssistant/Modules/Interfaces/IModuleBase.cs
@@ -3,6 +3,7 @@ using System;
 namespace HomeAssistant.Modules.Interfaces {
 	internal interface IModuleBase {
 		string ModuleIdentifier { get; set; }
+		string ModuleAuthor { get; set; }
 		Version ModuleVersion { get; set; }
 		(bool, T) InitModuleService<T>();
 		bool InitModuleShutdown();
