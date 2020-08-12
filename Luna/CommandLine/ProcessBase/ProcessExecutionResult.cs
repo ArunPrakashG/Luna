a8ea8c20cd3d886d17ecc2c4db00477d3848fd0e
Arun Prakash <arun.prakash.456789@gmail.com>
2020-08-13 00:05:03 +0530

    fluent process executor

---
+++ Luna/CommandLine/ProcessBase/ProcessExecutionResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Luna.CommandLine.ProcessBase {
+	public sealed class ProcessExecutionResult {
+		public readonly int ExitCode;
+		public readonly bool IsSuccessExitCode;
+		public readonly string StandardError;
+		public readonly string StandardOutput;
+		public readonly DateTime ExitedAt;
+
+		public ProcessExecutionResult(int exitCode, string stdError, string stdOut, DateTime exitedAt) {
+			ExitCode = exitCode;
+			IsSuccessExitCode = ExitCode == 0;
+			StandardError = stdError;
+			StandardOutput = stdOut;
+			ExitedAt = exitedAt;
+		}
+	}
+}
